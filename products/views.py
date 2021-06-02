aec70dddb04b5df461de6ffefb46ba6a2b7c0028
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-02 23:50:34 +0500

    добавил обработку json файлов goods.json и categories.json. сделал цикл for в шаблоне документа product.html для отображения

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+import json
+import os
+
 from django.shortcuts import render
 
-
+MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
 def index(request):
@@ -9,26 +12,33 @@ def index(request):
     }
     return render(request, 'products/index.html', context)
 
+
+
 def products(request):
-    context = {
-        'title': 'GeekShop',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090, 'describe': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'name': 'Синяя куртка The North Face', 'price': 23725, 'describe': 'гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390, 'describe': 'материал с плюшевой текстурой. Удобный и мягкий.'},
-            {'name': 'черный рюкзак Nike Heritage', 'price': 2340, 'describe': 'плотная ткань. Легкий материал.'},
-            {'name': 'черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590, 'describe': 'гладкий кожаный верх. Натуральный материал.'},
-            {'name': 'темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890, 'describe': 'дегкая эластичная ткань сирсакер Фактурная ткань.'},
-        ]
-    }
+    context = {'title': 'Geekshop-catalog'}
+    file_path = os.path.join(MODULE_DIR, 'fixtures/categories.json')
+    context['categories'] = json.load(open(file_path, encoding='utf-8'))
+
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context['products'] = json.load(open(file_path, encoding='utf-8'))
     return render(request, 'products/products.html', context)
+
+
 def news(request):
     return render(request, 'products/news.html')
+
+
 def clothes(request):
     return render(request, 'products/clothes.html')
+
+
 def shoes(request):
     return render(request, 'products/shoes.html')
+
+
 def features(request):
     return render(request, 'products/features.html')
+
+
 def gives(request):
     return render(request, 'products/gives.html')
