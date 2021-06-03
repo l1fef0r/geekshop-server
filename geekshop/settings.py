96622cc9ff738ca701b76345474ce5332ad22b58
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-03 18:19:27 +0500

    добавил админку

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'products',
 ]
 
@@ -123,6 +124,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
