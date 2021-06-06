0584f33d10ce0133aa41cae9166fd36198faae87
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-07 01:12:29 +0500

    добавил users, окно авторизации и регистрации

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    context = {'title': 'Geekshop-auth-register'}
+    return render(request, 'users/login.html', context)
+
+def register(request):
+    context = {'title': 'Geekshop-auth-register'}
+    return render(request, 'users/register.html', context)
