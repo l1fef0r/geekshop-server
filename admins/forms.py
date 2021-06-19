1e213e2b2cdb4e907cc9985b89e4b3c630958122
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-19 14:04:10 +0500

    создание пользователя

---
+++ admins/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from users.forms import UserRegisterForm
+from users.models import User
+
+class UserAdminRegisterForm(UserRegisterForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
+    model = User
+    fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
