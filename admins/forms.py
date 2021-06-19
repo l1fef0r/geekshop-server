7bd29b53d3f52b53fc4e168af708df9a7ed30620
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-19 14:46:18 +0500

    завкончил круд, добавил декоратор в круд

--- admins/forms.py
+++ admins/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
 class UserAdminRegisterForm(UserRegisterForm):
@@ -7,3 +7,7 @@ class UserAdminRegisterForm(UserRegisterForm):
 
     model = User
     fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': False}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': False}))
--- admins/views.py
+++ admins/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from users.models import User
-from admins.forms import UserAdminRegisterForm
+from django.contrib.auth.decorators import user_passes_test
 
+from users.models import User
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'admins/admin.html')
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     context = {'title': 'GeekShop - Admin | Users', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -23,8 +28,22 @@ def admin_users_create(request):
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'admins/admin-users-create.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, id):
-    return render(request, 'admins/admin-users-update-delete.html')
+    selected_users = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_users)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_users)
+    context = {'title': 'GeekShop - Admin | update user', 'form': form, 'selected_users': selected_users,}
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_delete(request, id):
-    pass
+    user = User.objects.get(id=id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reversed('admin:admin_users'))
