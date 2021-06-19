ad4a46969b3bcb8ad1693bdfd400c2a5ed9e9447
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-06-19 14:04:05 +0500

    создание пользователя

--- admins/views.py
+++ admins/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from users.models import User
+from admins.forms import UserAdminRegisterForm
 
 def index(request):
     return render(request, 'admins/admin.html')
 
-def admin_users(requset):
-    contex = {'users': User.objects.all()}
-    return render(requset, 'admins/admin-users-read.html', contex)
+def admin_users(request):
+    context = {'title': 'GeekShop - Admin | Users', 'users': User.objects.all()}
+    return render(request, 'admins/admin-users-read.html', context)
 
-def admin_users_create(requset):
-    return render(requset, 'admins/admin-users-create.html')
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegisterForm()
+    context = {'title': 'GeekShop - Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 def admin_users_update(request, id):
     return render(request, 'admins/admin-users-update-delete.html')
